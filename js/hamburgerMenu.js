16ae9e0cc3c4bc4b8821a168bbe8ab442d3e9d23
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-05-05 18:06:36 +0300

    dosyalar yüklendi

---
+++ js/hamburgerMenu.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.querySelector(".login-buttons");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
